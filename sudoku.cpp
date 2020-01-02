3ba0ccb06de95e79554ad8ad2ef45152d91d08ea
sylph3 <42126886+sylph3@users.noreply.github.com>
2020-01-02 23:28:21 +0800

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -7,17 +7,108 @@
 #include <fstream>
 #include <cmath>
 #include <string.h>
+#include <algorithm>
 
 using namespace std;
 
 int n;//需要生成的数独矩阵个数
-char grid[9][9];//生成的数独矩阵
-char buffer[1000000];//读控制台的缓冲
-ofstream OutCreateFile("../BIN/sudoku.txt");//生成矩阵写入的文件
+char grid[10][10];//生成的数独矩阵
+char buffer[170000000];//读矩阵到文件的缓冲
+int buffer_cursor = 0;//buffer数组的当前索引
+ofstream OutCreateFile("sudoku.txt");//生成矩阵写入的文件
+char permutation[15] = { '1','2','3','4','5','6','7','9' };//生成的初始排列
+char move1[10][5] = { "036","063" };//矩阵每三行的移动规则
+char move2[10][5] = { "258","285","528","582","852","825" };//移动矩阵4-6行
+char move3[10][5] = { "147","174","417","471","714","741" };//移动矩阵7-9行
 
 void CreateSudoku(int n)
 {
 	//生成数独具体代码
+	int cnt_create = 0;//已经生成的数独数目
+	//循环生成n个矩阵
+	while (cnt_create < n)
+	{
+		permutation[8] = '8';//学号位固定
+		//根据移动方式共有72种不同的排列
+		for (int i = 0; i < 2; ++i)
+		{
+			for (int j = 0; j < 6; ++j)
+			{
+				for (int k = 0; k < 6; ++k)
+				{
+					//每三行平移构建矩阵
+					//1-3行
+					for (int row = 0; row < 3; ++row)
+					{
+						//生成该行首字母
+						int tmp = (8 + move1[i][row] - '0') % 9;//生成平移后位置，因为学号位在末尾，故+8
+						buffer[buffer_cursor] = permutation[tmp];//存入缓冲区
+						buffer_cursor++;
+
+						//该行按数字和空格存入，m为该行字符计数
+						for (int m = 1; m <= 16; m++)
+						{
+							buffer[buffer_cursor++] = ' ';//存入空格
+							m++;
+							int tmp = (8 - (m / 2) + move1[i][row] - '0') % 9;//生成当前处理数平移后位置
+							buffer[buffer_cursor++] = permutation[tmp];
+						}
+						buffer[buffer_cursor++] = '\n';//换行
+					}
+
+					//4-6行
+					for (int row = 0; row < 3; ++row)
+					{
+						//生成该行首字母
+						int tmp = (8 + move2[i][row] - '0') % 9;//生成平移后位置，因为学号位在末尾，故+8
+						buffer[buffer_cursor] = permutation[tmp];//存入缓冲区
+						buffer_cursor++;
+
+						//该行按数字和空格存入，m为该行字符计数
+						for (int m = 1; m <= 16; m++)
+						{
+							buffer[buffer_cursor++] = ' ';//存入空格
+							m++;
+							int tmp = (8 - (m / 2) + move1[i][row] - '0') % 9;//生成当前处理数平移后位置
+							buffer[buffer_cursor++] = permutation[tmp];
+						}
+						buffer[buffer_cursor++] = '\n';//换行
+					}
+
+					//7-9行
+					for (int row = 0; row < 3; ++row)
+					{
+						//生成该行首字母
+						int tmp = (8 + move3[i][row] - '0') % 9;//生成平移后位置，因为学号位在末尾，故+8
+						buffer[buffer_cursor] = permutation[tmp];//存入缓冲区
+						buffer_cursor++;
+
+						//该行按数字和空格存入，m为该行字符计数
+						for (int m = 1; m <= 16; m++)
+						{
+							buffer[buffer_cursor++] = ' ';//存入空格
+							m++;
+							int tmp = (8 - (m / 2) + move1[i][row] - '0') % 9;//生成当前处理数平移后位置
+							buffer[buffer_cursor++] = permutation[tmp];
+						}
+						buffer[buffer_cursor++] = '\n';//换行
+					}
+
+					buffer[buffer_cursor++] = '\n';//两个不同矩阵间空一行
+
+					cnt_create++;//生成矩阵计数+1
+					if (cnt_create == n)
+						return;
+				}
+			}
+		}
+		
+		//按字典序生成全排列,但学号位不动
+		next_permutation(permutation, permutation + 8);
+	}
+
+	//输出到文件,有误，待修改 
+	OutCreateFile << buffer;
 }
 
 void SolveSudoku()
@@ -42,7 +133,9 @@ int main(int argc, char** argv)
 			cout << "Number is illegal" << endl;
 			return 0;
 		}
+		buffer_cursor = 0;
 		CreateSudoku(n);
+		printf("success!\n"); 
 	}
 	else if (strcmp(argv[1], "-s") == 0)
 	{
